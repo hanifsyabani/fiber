5992a9afa2db097a69a48167946ff1dedd1cbafd
Micinyoo <prawiranggacaesar@gmail.com>
2025-01-07 09:18:13 +0700

    Namaku Riko, seorang siswa SMP kelas IX asal Bogor. Aku memiliki sahabat bernama Andre, yang merupakan teman setia dan sekelas sejak di bangku sekolah dasar.
    Hari ini adalah hari Sabtu, di mana mata pelajaran hanya tiga mapel. Pada mapel terakhir, yakni Bahasa Inggris, semua berjalan seperti biasa. Hari telah menunjukkan pukul 13.20, tandanya sebentar lagi akan pulang.
    
    Tiba-tiba pintu kelas diketok oleh seseorang. Ketika Bu Guru membukanya, ternyata yang datang adalah Pak Imron, pamannya Andre, adek kandung dari ibunya

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,64 @@
-import Image from "next/image";
+'use client'
+import { useState } from "react";
 
-export default function Home() {
+
+const App = () => {
+  const [color, setColor] = useState("#ffffff");
+  const [count, setCount] = useState(0);
+
+  const generateRandomColor = () => {
+    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0")}`;
+    setColor(randomColor);
+    setCount(count + 1);
+  };
+
+  // const generateCounter = () => {
+  
+
   return (
     <div>
-      Hello World
+    <div>
+      <h1 style={styles.title}>Generator Warna Acak</h1>
+      <p style={styles.colorCode}>Kode Warna: {color}</p>
+      <p style={styles.colorCode}>Time Clicked : {count}</p>
+      <button style={styles.button} onClick={generateRandomColor}>
+        Hasilkan Warna
+      </button>
+    </div>
     </div>
   );
-}
+};
+
+const styles = {
+  container: {
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    fontFamily: "Arial, sans-serif",
+    color: "#333",
+    transition: "background-color 0.5s ease",
+  },
+  title: {
+    fontSize: "2rem",
+    marginBottom: "10px",
+  },
+  colorCode: {
+    fontSize: "1.2rem",
+    marginBottom: "20px",
+  },
+  button: {
+    padding: "10px 20px",
+    fontSize: "1rem",
+    backgroundColor: "#007BFF",
+    color: "white",
+    border: "none",
+    borderRadius: "5px",
+    cursor: "pointer",
+    transition: "background-color 0.3s ease",
+  },
+};
+
+export default App;
+
